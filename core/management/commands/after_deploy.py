abd92c3c119a023adac0935f473c74aa8c87415d
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-10 14:06:18 +0200

    run syncdb and migrate after deploy

--- core/management/commands/after_deploy.py
+++ core/management/commands/after_deploy.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand
 from core.models import Follow
 from django.contrib.auth import get_user_model
-# from django.core.management import call_command
+from django.core.management import call_command
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        # call_command('syncdb', interactive=False)
-        # call_command('migrate', interactive=False)
+        call_command('syncdb', interactive=False)
+        call_command('migrate', interactive=False)
         admin = get_user_model().objects.get(username="admin")
         Follow.objects.create(user=admin, target=admin)
