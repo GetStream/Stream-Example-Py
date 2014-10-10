3e75a8340b20f00c9a76e9fb66f99e166c81ab48
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-10 13:33:01 +0200

    update app.json flow

--- core/management/commands/after_deploy.py
+++ core/management/commands/after_deploy.py
@@ -1,12 +1,13 @@
 from django.core.management.base import BaseCommand
-from stream_django.feed_manager import feed_manager
-from stream_django import Follow
+from core.models import Follow
 from django.contrib.auth import get_user_model
+# from django.core.management import call_command
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        # call_command('syncdb', interactive=False)
+        # call_command('migrate', interactive=False)
         admin = get_user_model().objects.get(username="admin")
         Follow.objects.create(user=admin, target=admin)
-        feed_manager.follow_user(admin, admin)
