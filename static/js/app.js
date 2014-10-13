ea360fc5c6ff5d41ef68f0e7fc88f080eb143115
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-10-13 16:59:14 +0200

    basic notification example

--- static/js/app.js
+++ static/js/app.js
@@ -1,65 +1,71 @@
-var App = function () {
-    this.initialize.apply(this, arguments);
+var App = function() {
+	this.initialize.apply(this, arguments);
 };
 
 App.prototype = {
-    initialize: function() {
-        var that = this;
-        $('form.create-destroy').submit($.proxy(this.toggleFormSubmit, this));
-    },
-    lockForm: function($form) {
-        var $btn = $form.find('input[type="submit"]');
-        $btn.prop('disabled', true);
-    },
-    toggleFormState: function() {
-        var $btn = this.find('input[type="submit"]');
-        $btn.prop('disabled', false);
-        var text = $btn.val();
-        var toggle = $btn.data('toggle');
-        $btn.data('toggle', text);
-        $btn.val(toggle);
-        if (this.find('input[name="remove"]').length > 0) {
-            this.find('input[name="remove"]').remove();
-            $btn.removeClass('btn-danger');
-        }
-        else {
-            this.append('<input name="remove" type="hidden" value="1"> ');
-            $btn.addClass('btn-danger');
-        }
-    },
-    toggleFormSubmit: function(event) {
-        var $form = $(event.target);
-        $.ajax({
-            type: "POST",
-            url: $form.attr('action'),
-            data: $form.serialize(),
-            success: $.proxy(this.toggleFormState, $form)
-        });
-        this.lockForm($form);
-        return false;
-    }
+	initialize : function() {
+		var that = this;
+		$('form.create-destroy').submit($.proxy(this.toggleFormSubmit, this));
+		$('#notification-toggle').click(function() {
+			$.get("/notification_feed/", function(data) {
+				$("#notification-container").html(data);
+			});
+		});
+	},
+	lockForm : function($form) {
+		var $btn = $form.find('input[type="submit"]');
+		$btn.prop('disabled', true);
+	},
+	toggleFormState : function() {
+		var $btn = this.find('input[type="submit"]');
+		$btn.prop('disabled', false);
+		var text = $btn.val();
+		var toggle = $btn.data('toggle');
+		$btn.data('toggle', text);
+		$btn.val(toggle);
+		if (this.find('input[name="remove"]').length > 0) {
+			this.find('input[name="remove"]').remove();
+			$btn.removeClass('btn-danger');
+		} else {
+			this.append('<input name="remove" type="hidden" value="1"> ');
+			$btn.addClass('btn-danger');
+		}
+	},
+	toggleFormSubmit : function(event) {
+		var $form = $(event.target);
+		$.ajax({
+			type : "POST",
+			url : $form.attr('action'),
+			data : $form.serialize(),
+			success : $.proxy(this.toggleFormState, $form)
+		});
+		this.lockForm($form);
+		return false;
+	}
 };
 
 window.app = new App();
-app.Realtime = function () {
-    this.initialize.apply(this, arguments);
+app.Realtime = function() {
+	this.initialize.apply(this, arguments);
 };
 
 app.Realtime.prototype = {
-    initialize: function(feedId, token, elementIdentifier) {
-    	this.feedId = feedId;
-    	this.token = token;
-    	this.elementIdentifier = elementIdentifier;
-    	this.client = streamClient;
-    	this.feed = this.client.feed(feedId, this.token);
-    	var scope = this;
-    	function changeBound() {
-    		return scope.changed.apply(scope, arguments);
-    	}
-    	this.feed.subscribe(changeBound);
-    	this.element = $(elementIdentifier);
+	initialize : function(feedId, token, elementIdentifier) {
+		this.feedId = feedId;
+		this.token = token;
+		this.elementIdentifier = elementIdentifier;
+		this.client = streamClient;
+		this.feed = this.client.feed(feedId, this.token);
+		var scope = this;
+		function changeBound() {
+			return scope.changed.apply(scope, arguments);
+		}
+
+
+		this.feed.subscribe(changeBound);
+		this.element = $(elementIdentifier);
 	},
-	changed: function(data) {
+	changed : function(data) {
 		var unseen = data.unseen;
 		this.element.html(unseen);
 		if (unseen == 0) {
