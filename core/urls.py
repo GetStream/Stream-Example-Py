84d5bde06b6433061e2c3ff977f7add3a64f0603
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-29 11:43:58 +0100

    serve media files via django

--- core/urls.py
+++ core/urls.py
@@ -33,10 +33,9 @@ urlpatterns = patterns('',
                        url(r'^auth/', include('django.contrib.auth.urls')),
                        )
 
-if settings.DEBUG:
-    urlpatterns = patterns('',
-                           url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-                               {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-                           url(r'', include(
-                               'django.contrib.staticfiles.urls')),
-                           ) + urlpatterns
+urlpatterns = patterns('',
+                       url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                           {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+                       url(r'', include(
+                           'django.contrib.staticfiles.urls')),
+                       ) + urlpatterns
